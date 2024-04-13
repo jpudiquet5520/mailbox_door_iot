5b0325c19543f6b62cbc5cdb4cf11b8813cb00f3
jpudiquet5520 <jpudiquet5520@sdsu.edu>
2024-04-13 00:27:17 -0700

    Improving readability in mailbox_iot.ino

--- mailbox_iot/mailbox_iot.ino
+++ mailbox_iot/mailbox_iot.ino
@@ -2,6 +2,7 @@
 #include <PubSubClient.h>
 #include <HTTPClient.h>
 #include <time.h>
+
 /*
  * wifi_info.h includs wifi ssid, passwords, mqtt broker info, customize included wifi_info_sample.h and
  * save as wifi_info.h in this same folder.
@@ -22,6 +23,7 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 HTTPClient http;  //for posting
+
 //Define the door Sensor PIN and initial state (Depends on your reed sensor N/C  or N/O )
 gpio_num_t  doorSensorPIN = GPIO_NUM_12;  // Reed GPIO PIN: //CHANGE TO WHATEVER PINOUT WE END UP USING
 gpio_num_t GPIO_INPUT_IO_TRIGGER = doorSensorPIN;
@@ -41,55 +43,57 @@ char total_time_awake[21];  //timing holds string h:m:s for time door is opened.
 long rssi =0;
 
 void setup_wifi() {
-    start_time=millis();
+    start_time=millis(); // Start time is assigned the current number of milliseconds since the ESP32 started running
  
     // We start by connecting to a WiFi network
     Serial.println();
     Serial.print("Connecting to ");
     Serial.println(wifi_ssid);
-    WiFi.disconnect();
+    WiFi.disconnect(); // Disconnects any existing WiFi connections that the ESP32 might have.
 
     Serial.printf("Trying to connec to WiFi: %s, p/w: %s ... \n",wifi_ssid,wifi_password);
-    WiFi.begin(wifi_ssid, wifi_password);
+    WiFi.begin(wifi_ssid, wifi_password); //Initializing the connection to the WiFi network
 
-    int count=0;
+    int count=0; // Control the formatting of output dots, indicating ongoing connection attempts.
 
+    // This loop continues until the WiFi is connected.
     while (WiFi.status() != WL_CONNECTED) {
-    
-        count++;
-    if ( count % 40==0 )
-        Serial.print("\n");
-    else
-        Serial.print(".");
+            count++;
+        if ( count % 40==0 )
+            Serial.print("\n");
+        else
+            Serial.print(".");
 
-    delay(25);
+        delay(25); // Pauses loop for 25ms between each iteration; for stabalization
+        
+        // Did wifi fail to connect then time out?
+        if (millis()- start_time > wifi_timeout ) 
+        {
+            Serial.println("Wifi Failed to connect ..timeout..");
+
+            //TODO: Uncomment this
+            //esp32_sleep(); // Puts the ESP32 into a low-power sleep mode to conserve battery.
+        }
     
-    if (millis()- start_time > wifi_timeout )  //did wifi fail to connect then time out
-    {
-        Serial.println("Wifi Failed to connect ..timeout..");
-        //esp32_sleep();
-    }
-    
-}
+    }//End while()
  
-    rssi= WiFi.RSSI();
+    rssi= WiFi.RSSI(); // RSSI(Received Signal Strength Indicator); Quality of WiFi Connection
     Serial.println( "\n WiFi connected in " +(String)(millis()- start_time ) + "ms to AP:"+wifi_ssid+ " Rssi:"+(String)rssi );
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-}
+}//End setup_wifi()
 
 
 void setup() {
     // Your setup code here
     Serial.begin(115200); // Set the baud rate (data rate over serial communication) to match baud rate in serial monitor
     delay(1000); //Delay for the serial communication to stabalize
-    //TODO: NEED TO COMMENT THIS OUT, FOR TROUBLESHOOTING PURPOSES
-    //Needed to call the setup_wifi() function that is why we couldn't 
-    //setup_wifi();  
     
-    Serial.println("Hello World");
+    //TODO: NEED TO COMMENT THESE OUT, FOR TROUBLESHOOTING setup_wifi()
+    //setup_wifi();  
+    //Serial.println("Hello World");
+
     Serial.println("Serial initialized."); // Check if Serial communication is working
-    //setup_wifi();
 }
 
 void loop() {
