9acef3e5860c2ddc982191ba6c29810886949c2d
jpudiquet5520 <jpudiquet5520@sdsu.edu>
2024-04-12 19:51:36 -0700

    changed test file names and mv to test folder

---
+++ component_tests/reed_test/reed_test.ino
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+const int reedPin = 23; // GPIO pin connected to the reed switch
+
+void setup() {
+  Serial.begin(115200);          // Start the Serial communication
+  pinMode(reedPin, INPUT_PULLUP); // Set the reed switch pin as input with internal pull-up resistor
+}
+
+void loop() {
+  int reedState = digitalRead(reedPin); // Read the state of the reed switch
+  if (reedState == LOW) { // If the reed switch is closed (magnet detected)
+    Serial.println("Magnet Detected!");
+  } 
+  else {
+    Serial.println("No Magnet Detected.");
+  }
+  delay(500); // Delay to prevent spamming (adjust as necessary)
+}
+
